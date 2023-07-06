db0977f8aacba9b2a0b8cd128e9944327a153ce2
qiuqfang <qiuqfang@outlook.com>
2023-07-06 10:33:39 +0800

    chore: add type

--- src/router/withRouter.tsx
+++ src/router/withRouter.tsx
@@ -1,17 +1,28 @@
+import { ComponentClass, FC } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import type { Location, NavigateFunction, Params } from "react-router-dom";
+
+export type WithRouterProp = {
+  router: {
+    location: Location;
+    params: Readonly<Params<string>>;
+    navigate: NavigateFunction;
+  };
+};
+export type WithRouterComponentProp<T> = T & WithRouterProp;
 
 /**
  * 解决使用类组件时无法使用路由功能
- * @param {T} Component
- * @returns NewComponent
  */
-export const withRouter = (Component: any) => {
-  function WithRouterPropComponent(props: any) {
+export const withRouter = <P, S>(
+  RouterComponent: ComponentClass<WithRouterComponentProp<P>, S>
+): FC<P> => {
+  function WithRouterPropComponent(props: P) {
     const location = useLocation();
     const params = useParams();
     const navigate = useNavigate();
 
-    return <Component {...props} router={{ location, params, navigate }}></Component>;
+    return <RouterComponent {...props} router={{ location, params, navigate }}></RouterComponent>;
   }
   return WithRouterPropComponent;
 };
