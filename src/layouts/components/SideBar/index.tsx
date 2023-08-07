d1c1c1200d70f03b0751bb5b31f3e2833c53dd77
qiuqfang <qiuqfang@outlook.com>
2023-08-07 09:13:28 +0800

    chore: sidebar useCallback

--- src/layouts/components/SideBar/index.tsx
+++ src/layouts/components/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import MenuRoute from "@/components/MenuRoute";
 
 import "./index.less";
@@ -26,9 +26,9 @@ function SideBar() {
       <aside
         className="sidebar relative dark:bg-[#141414] max-sm:absolute max-sm:right-0"
         style={{ width: sideBarState.width }}
-        onClick={(e) => {
+        onClick={useCallback((e) => {
           e.stopPropagation();
-        }}
+        }, [])}
       >
         <MenuRoute width={sideBarState.width} inlineCollapsed={!sideBarState.open} />
         <Icon
@@ -36,13 +36,13 @@ function SideBar() {
           className={`absolute bottom-1 cursor-pointer max-sm:hidden ${
             sideBarState.open ? "right-1 rotate-0" : "left-1/2 ml-[-12px] rotate-180"
           }`}
-          onClick={() => {
+          onClick={useCallback(() => {
             if (sideBarState.open) {
               setSideBarState({ width: 50, open: false });
             } else {
               setSideBarState({ width: 208, open: true });
             }
-          }}
+          }, [sideBarState.open])}
         />
       </aside>
     </div>
